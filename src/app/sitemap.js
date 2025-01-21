1ba998fadc174a6bf9766713075cc5b404d6d573
oxiadenine <sga.samuel@gmail.com>
2025-01-22 00:43:09 +0100

    Add sitemap and robots functions

---
+++ src/app/sitemap.js
@@ -0,0 +1,17 @@
+import { getKnowledge } from "@/data/knowledge-fetcher";
+
+export default async function sitemap() {
+  const knowledge = await getKnowledge();
+
+  return [{ 
+    url: process.env.URL, 
+    lastModified: new Date(),
+    changeFrequency: "yearly", 
+    priority: 1 
+  }].concat(knowledge.map(({ data }) => ({
+    url: `${process.env.URL}/knowledge/${data.subject}`,
+    lastModified: new Date(data.date),
+    changeFrequency: "yearly",
+    priority: 1
+  })));
+}
