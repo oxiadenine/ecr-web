8f60c3398b8890092c0474b05cc6e3bdbf522f3d
oxiadenine <sga.samuel@gmail.com>
2025-01-22 00:00:41 +0100

    Add knowledge subject page

---
+++ src/app/knowledge/[subject]/page.js
@@ -0,0 +1,45 @@
+import Header from "@/app/components/header";
+import Footer from "@/app/components/footer";
+import styles from "@/app/knowledge/[subject]/page.module.css";
+import { getKnowledgeOf } from "@/data/knowledge-fetcher";
+
+export async function generateMetadata({ params }) {
+  const subject = (await params).subject;
+
+  const knowledge = await getKnowledgeOf(subject);
+
+  return {
+    title: knowledge.data.title,
+    description: knowledge.data.title,
+    keywords: knowledge.data.subject.split("-"),
+    authors: [{ name: knowledge.data.author }],
+    openGraph: {
+      title: knowledge.data.title,
+      description: knowledge.data.title,
+      url: `${process.env.URL}/knowledge/${knowledge.data.subject}`,
+      images: knowledge.data.images.map(image => ({ 
+        url: `${process.env.URL}${image.src}`, alt: image.alt 
+      }))
+    }
+  };
+}
+
+export default async function Page({ params }) {
+  const subject = (await params).subject;
+
+  const { component: Knowledge } = await getKnowledgeOf(subject);
+
+  return (
+    <>
+      <Header />
+      <div>
+        <main>
+          <div className={styles["page"]}>
+            <Knowledge />
+          </div>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
