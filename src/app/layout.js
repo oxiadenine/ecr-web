b19239b1d23d37bb7f134a91218614ea84375eb2
oxiadenine <sga.samuel@gmail.com>
2025-01-21 23:32:15 +0100

    Fix FA scripts touching the DOM by replacing it with web font icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-import Script from "next/script";
 import { orbitron } from "@/app/fonts";
 import "@/app/globals.css";
 import styles from "@/app/layout.module.css";
@@ -13,9 +12,6 @@ export default function Layout({ children }) {
           <div>{children}</div>
         </div>
       </body>
-      <Script src="/js/fa-icons.min.js" />
-      <Script src="/js/fa-icons-solid.min.js" />
-      <Script src="/js/fa-icons-brands.min.js" />
     </html>
   );
 }
