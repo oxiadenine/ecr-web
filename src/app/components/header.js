bded3824e744695a97f75174a257e5bdc294d396
oxiadenine <sga.samuel@gmail.com>
2025-01-21 23:38:02 +0100

    Add knowledge search to home page

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,8 +1,14 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { monoton } from "@/app/fonts";
+import SearchInput from "@/app/components/search-input";
 import styles from "@/app/components/header.module.css";
 
 export default function Header() {
+  const pathname = usePathname();
+
   return (
     <header>
       <div className={styles["header"]}>
@@ -10,6 +16,7 @@ export default function Header() {
           <img src="/ecr-logo.png" alt="logo" />
           <h3 className={monoton.variable}>El Chanchito Rey</h3>
         </Link>
+        {pathname == "/" && <SearchInput placeholder="Buscar.." />}
       </div>
     </header>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,13 @@ import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
 import KnowledgeCard from "@/app/components/knowledge-card";
 import styles from "@/app/page.module.css";
-import { getKnowledge } from "@/data/knowledge-fetcher";
+import { getKnowledgeBy } from "@/data/knowledge-fetcher";
 
-export default async function Page() {
-  const knowledge = await getKnowledge();
+export default async function Page(props) {
+  const searchParams = await props.searchParams;
+  const searchTerm = searchParams?.s || "";
+
+  const knowledge = await getKnowledgeBy(searchTerm);
 
   return (
     <>
