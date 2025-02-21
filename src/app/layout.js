4410c051284071a79b8e6ffe7c7274ba798bcb5f
oxiadenine <sga.samuel@gmail.com>
2025-02-21 13:57:31 +0100

    Add env to control analytics enablement

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,10 +6,12 @@ import styles from "@/app/layout.module.css";
 export const metadata = { title: "El Chanchito Rey" };
 
 export default function Layout({ children }) {
+  const isAnalyticsEnabled = !!+Bun.env.ANALYTICS_ENABLE ?? false;
+
   return (
     <html lang="es" className={orbitron.variable}>
       <body>
-        <Analytics />
+        {isAnalyticsEnabled && <Analytics />}
         <div className={styles["layout"]}>
           <div>{children}</div>
         </div>
