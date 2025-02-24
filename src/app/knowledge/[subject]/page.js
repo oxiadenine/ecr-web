645e0c8328722b05b1ec7cd88bfcb2f0620e2238
oxiadenine <sga.samuel@gmail.com>
2025-02-24 13:44:38 +0100

    Fix knowledge page throws error when knowledge not found

--- src/app/knowledge/[subject]/page.js
+++ src/app/knowledge/[subject]/page.js
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
 import styles from "@/app/knowledge/[subject]/page.module.css";
@@ -6,28 +7,28 @@ import { getKnowledgeOf } from "@/data/knowledge-fetcher";
 export async function generateMetadata({ params }) {
   const subject = (await params).subject;
 
-  const knowledge = await getKnowledgeOf(subject);
+  const data = (await getKnowledgeOf(subject))?.data ?? null;
 
-  return {
-    title: knowledge.data.title,
-    description: knowledge.data.title,
-    keywords: knowledge.data.tags?.split(",") ?? [],
-    authors: [{ name: knowledge.data.author }],
+  return data ? {
+    title: data.title,
+    description: data.title,
+    keywords: data.tags?.split(",") ?? [],
+    authors: [{ name: data.author }],
     openGraph: {
-      title: knowledge.data.title,
-      description: knowledge.data.title,
-      url: `${Bun.env.URL}/knowledge/${knowledge.data.subject}`,
-      images: knowledge.data.images.map(image => ({
+      title: data.title,
+      description: data.title,
+      url: `${Bun.env.URL}/knowledge/${data.subject}`,
+      images: data.images.map(image => ({
         url: `${Bun.env.URL}${image.src}`, alt: image.alt
       }))
     }
-  };
+  } : null;
 }
 
 export default async function Page({ params }) {
   const subject = (await params).subject;
 
-  const { component: Knowledge } = await getKnowledgeOf(subject);
+  const { component: Knowledge } = await getKnowledgeOf(subject) ?? notFound();
 
   return (
     <>
